01186812911f439b24d33f6b23df9dbf74b1aac2
simonlaffut87 <simonlaffut87@gmail.com>
2025-09-06 12:35:48 +0200

    Updated MemberDashboard.tsx

--- src/components/MemberDashboard.tsx
+++ src/components/MemberDashboard.tsx
@@ -986,7 +986,7 @@ export function MemberDashboard({ user, onLogout }: MemberDashboardProps) {
                   radius={[2, 2, 0, 0]}
                 />
                 <Bar 
-                  dataKey="injection_residuelle" 
+                  dataKey="injection_complementaire" 
                   name="Injection Réseau" 
                   fill="#8B5CF6"
                   radius={[2, 2, 0, 0]}
