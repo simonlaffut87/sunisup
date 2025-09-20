9b0c0e972ab7688397bd232658a541578eb0710f
simonlaffut87 <simonlaffut87@gmail.com>
2025-09-20 15:17:59 +0200

    Fix HelmetProvider import error

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
 import App from './App.tsx';
 import './index.css';
 import './i18n';
