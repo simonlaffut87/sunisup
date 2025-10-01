c6d3605f53e474d19378f13de71296563263ddf7
simonlaffut87 <simonlaffut87@gmail.com>
2025-10-01 17:11:15 +0200

    Increase logo size by 50%

--- src/App.tsx
+++ src/App.tsx
@@ -187,7 +187,7 @@ function App() {
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex items-center justify-between h-16">
               <Link to="/" className="flex items-center space-x-3">
-                <img src="/images/logo-v2.png" alt="Sun Is Up" className="w-16 h-16" />
+                <img src="/images/logo-v2.png" alt="Sun Is Up" className="w-24 h-24" />
               </Link>
 
               <div className="hidden md:flex items-center space-x-8">
